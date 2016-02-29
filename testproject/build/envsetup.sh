958dbb44c3c4f3d3e4e8988b6311e694e20ac3ce
Young-Ho Cha <ganadist@gmail.com>
2016-02-29 10:20:30 +0900

    testproject: add cm style target
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- testproject/build/envsetup.sh
+++ testproject/build/envsetup.sh
@@ -2,4 +2,4 @@ lunch() {
   export ANDROID_PRODUCT_OUT=${OUT_DIR}/target/product/generic
 }
 
-export LUNCH_MENU_CHOICES=(generic-userdebug aosp_arm-userdebug aosp_mako-userdebug aosp_hammerhead-userdebug)
+export LUNCH_MENU_CHOICES=(generic-userdebug aosp_arm-userdebug aosp_mako-userdebug aosp_hammerhead-userdebug cm_mako-userdebug)
