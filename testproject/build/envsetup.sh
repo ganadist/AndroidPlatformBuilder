4ab4b0a44ce673ef2c30f15db3b633beb1e21622
Young-Ho Cha <ganadist@gmail.com>
2016-03-01 16:04:26 +0900

    testproject: fix envsetup.sh
    ANDROID_PRODUCT_OUT must be abspath
    
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- testproject/build/envsetup.sh
+++ testproject/build/envsetup.sh
@@ -1,5 +1,5 @@
 lunch() {
-  export ANDROID_PRODUCT_OUT=${OUT_DIR}/target/product/generic
+  export ANDROID_PRODUCT_OUT=${PWD}/${OUT_DIR}/target/product/generic
 }
 
 export LUNCH_MENU_CHOICES=(generic-userdebug aosp_arm-userdebug aosp_mako-userdebug aosp_hammerhead-userdebug cm_mako-userdebug)
